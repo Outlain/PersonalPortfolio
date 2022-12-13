a06822a121a860b411bdc49adac8f84f53a1f793
Carlos Amado <exchangeside@gmail.com>
2022-12-13 03:14:13 -0500

    added text redabiility and created new project functionality

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,9 +2,11 @@ export const ProjectCard = ({ title, description, imgUrl, link }) => {
     return (
         <div className="project-inner">
             <a href={link}>
-                <div className="project-inner-inner" style={{ backgroundImage: `url(${imgUrl})`, backgroundSize: 'cover' }}>
-                    <h2>{title}</h2>
-                    <h5>{description}</h5>
+                <div className="project-inner-inner" style={{ backgroundImage: `url(${imgUrl})`}}>
+                    <div className="project-inner-inner-blur">
+                        <h2>{title}</h2>
+                        <h5>{description}</h5>
+                    </div>
                 </div>
             </a>
         </div>
